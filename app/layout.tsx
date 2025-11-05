66c600f934af885ea92b4c6c1ed64bef21e7b848
Nicole Morelos Cárdenas <nicmocard@gmail.com>
2025-11-05 01:13:09 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,20 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "Nicole M.Cárdenas | Portfolio",
+    template: "%s | Nicole M.Cárdenas",
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description:
+    "Full-Stack Developer specialized in NestJS, TypeScript, and PostgreSQL — blending technology, design, and innovation.",
   openGraph: {
-    title: "chronark.com",
+    title: "Nicole M.Cárdenas | Portfolio",
     description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+      "Full-Stack Developer specialized in NestJS, TypeScript, and PostgreSQL — blending technology, design, and innovation.",
+    url: "https://portfolio-nicolemcardenas.vercel.app",
+    siteName: "Nicole M.Cárdenas",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "/og.png",
         width: 1920,
         height: 1080,
       },
@@ -37,14 +38,11 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
-  twitter: {
-    title: "Chronark",
-    card: "summary_large_image",
-  },
   icons: {
     shortcut: "/favicon.png",
   },
 };
+
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
