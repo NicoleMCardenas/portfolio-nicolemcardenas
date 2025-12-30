94fe2d9b74488d388a256afd57a703a258c080a4
Nicole Morelos Cárdenas <nicmocard@gmail.com>
2025-12-29 18:23:02 -0600

    Update imp.page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,8 +2,8 @@
 import Link from "next/link";
 import React from "react";
 import { allProjects } from "contentlayer/generated";
-import { Navigation } from "../components/nav";
-import { Card } from "../components/card";
+import { Navigation } from "@/app/components/nav";
+import { Card } from "@/app/components/card";
 import { Article } from "./article";
 import { Eye } from "lucide-react";
 
