f1583bfe552b5e2fb3be225077e910c9ff762291
Nicole Morelos Cárdenas <nicmocard@gmail.com>
2025-12-29 23:31:14 -0600

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -8,6 +8,7 @@ import { Article } from "./article";
 import { Redis } from "@upstash/redis";
 import { Eye } from "lucide-react";
 
+export const runtime = "nodejs";
 export const revalidate = 60;
 
 function hasUpstashEnv() {
